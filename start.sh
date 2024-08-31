0eccb594afe6b4758742cdd7b352916afffcbd91
Pedro Domingues <pedro.domingues.pt@gmail.com>
2024-08-31 11:39:45 +0100

    Fix

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 
-docker compose down && docker compose build && docker compose up -d && docker compose logs -f -t
+docker compose down --remove-orphans && docker compose build && docker compose up -d && docker compose logs -f -t
