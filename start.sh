5f87fd06274e1b52406b7ea0639bdf674b3c1aa0
Pedro Domingues <pedro.domingues.pt@gmail.com>
2024-08-31 16:16:11 +0100

    Fix

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 
-mkdir mods_dust216
-mkdir mods_dust2csgo
+mkdir -p mods_dust216
+mkdir -p mods_dust2csgo
 
 chmod -R 777 *
 
