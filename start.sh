88c2bac712ecf10c06b13e6b8627156712df6ddf
Pedro Domingues <pedro.domingues.pt@gmail.com>
2024-08-31 10:24:39 +0100

    Making sure host machine is ipv4 only

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+
 docker compose down && docker compose build && docker compose up -d && docker compose logs -f -t
