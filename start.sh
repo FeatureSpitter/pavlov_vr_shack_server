cdeeba5b00ac4fd1e73391287ec63520a533e644
Pedro Domingues <pedro.domingues.pt@gmail.com>
2024-08-31 15:29:32 +0100

    Fix

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 
+mkdir mods_dust216
+mkdir mods_dust2csgo
+
 chmod -R 777 mods_dust2*
 
 docker compose down --remove-orphans && docker compose build && docker compose up -d && docker compose logs -f -t
