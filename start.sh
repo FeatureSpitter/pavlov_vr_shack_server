12af56212573655a4b6a9f70d7dc16919bb76c62
Pedro Domingues <pedro.domingues.pt@gmail.com>
2024-09-02 15:07:23 +0100

    Adding halo server

--- start.sh
+++ start.sh
@@ -4,9 +4,10 @@ sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 
-mkdir -p mods_dust216
+mkdir -p mods_dust2cs16
 mkdir -p mods_dust2csgo
+mkdir -p mods_halo
 
-chmod -R 777 *
+chmod -R 755 *
 
 docker compose down --remove-orphans && docker compose build && docker compose up -d && docker compose logs -f -t
